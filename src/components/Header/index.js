822880090127f40e72f85845c89b3cd81c1e1086
Chris Hadley <hadleyc15@yahoo.com>
2020-09-12 16:22:45 -0600

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header(props) {
 
   return (
     <header className="flex-row space-between width:100%">
-      <h1>Christopher Hadley</h1>
+      <h1>Chris Hadley</h1>
       {props.children}
     </header>
   );
