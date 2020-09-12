af9746e8c73601f0e81c2db1871db4188e3c2d59
Chris Hadley <hadleyc15@yahoo.com>
2020-09-12 16:18:50 -0600

    Updates

---
+++ precache-manifest.7f30833121d0789085ab9d8dd0e0b6d0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e58b42ae21525809b9ba475c355c3c4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "859ad60b18ad3f87e7d2",
+    "url": "/react-portfolio/static/css/main.6305de34.chunk.css"
+  },
+  {
+    "revision": "8cb954078d1bff15e93b",
+    "url": "/react-portfolio/static/js/2.2193c0ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.2193c0ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859ad60b18ad3f87e7d2",
+    "url": "/react-portfolio/static/js/main.e3950430.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "ff579a188713af65108acd85482600ca",
+    "url": "/react-portfolio/static/media/Budget-Tracker.ff579a18.jpg"
+  },
+  {
+    "revision": "54bf145e480262ace03762161695e13c",
+    "url": "/react-portfolio/static/media/Campground-Finder.54bf145e.jpg"
+  },
+  {
+    "revision": "da1f110e0b4bc8b37006ca2fa5d0c7b4",
+    "url": "/react-portfolio/static/media/Coding-Quiz.da1f110e.jpg"
+  },
+  {
+    "revision": "4ae5affb2d1dd7ee4548a0345db5e643",
+    "url": "/react-portfolio/static/media/Tech-Blog.4ae5affb.jpg"
+  },
+  {
+    "revision": "f693ed0e5cb51298f920bf40549cb3cc",
+    "url": "/react-portfolio/static/media/Weather-Dashboard.f693ed0e.jpg"
+  },
+  {
+    "revision": "ede58d16071c6da1dd53f285e6929ad2",
+    "url": "/react-portfolio/static/media/What-2-Watch.ede58d16.jpg"
+  },
+  {
+    "revision": "54a278dddac465787cbd0339f8a89d05",
+    "url": "/react-portfolio/static/media/family-3.54a278dd.jpg"
+  }
+]);
