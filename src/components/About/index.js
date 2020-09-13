b030475bb752205171fc47b6cb7046180af20637
Chris Hadley <hadleyc15@yahoo.com>
2020-09-12 19:22:02 -0600

    small wording update to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ function About() {
         Full time husband and father currently working in healthcare as a Dialysis Technician. Recently graduated from the University of Utah's Full Stack Web Development Bootcamp. My background is in healthcare. I have been working as a Dialysis Technician for four years.  Those four years in dialysis have taught me attention to detail as well as great communication skills.
         </p>
         <p>
-         I love to solve puzzles and to push myself to understand not only the solution but the solving process as well.  I have a great desire to learn as well as to help others as well.  Coding has always interested me due to the problem solving nature as well as the fast paced environment that comes with it.
+         I love to solve puzzles and to push myself to understand not only the solution but the solving process as well.  I have a great desire to learn as well as to help others.  Coding has always interested me due to the problem solving nature and the fast paced environment that comes with it.
         </p>
       </div>
     </section>
